eb7bd5d16bc33e6ddad02acc50fef3cf0963208e
Anil Punjabi <anilcool@gmail.com>
2023-11-11 17:26:20 -0800

    Connected PlayerHealth and Stamina images via script

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
-    [Header("Settings")]
-    [SerializeField] private float maxHealth;
+
+    private float maxHealth = 1.0f;
+    private float maxStamina = 1.0f;
+
+
     private float currentHealth;
+    private float currentStamina;
+ 
+    [SerializeField]
+    private UnityEngine.UI.Image _healthBarForegroundImage;
+
+    [SerializeField]
+    private UnityEngine.UI.Image _staminaBarForegroundImage;
 
     private void Awake() {
-        currentHealth = maxHealth;
+        currentHealth = 0.7f;
+        currentStamina = 0.7f;
     }
 
     public bool TakeDamage(float damage) {
@@ -20,7 +34,33 @@ public class PlayerHealth : MonoBehaviour {
         return false;
     }
 
+    public bool LoseStamina(float staminaLost)
+    {
+        currentStamina -= staminaLost;
+        if (currentStamina <= 0)
+        {
+            Debug.Log("Player has no stamina");
+            return true;
+        }
+        Debug.Log("Player is now at " + currentStamina + " stamina.");
+        return false;
+    }
+
     private void Die() {
         Destroy(gameObject);
+    }
+
+    void LateUpdate()
+    {
+        UpdateHealthAndStamina();
+    }
+
+    private void UpdateHealthAndStamina()
+    {
+        if (_healthBarForegroundImage.fillAmount != currentHealth)
+            _healthBarForegroundImage.fillAmount = currentHealth;
+
+        if (_staminaBarForegroundImage.fillAmount != currentStamina)
+            _staminaBarForegroundImage.fillAmount = currentStamina;
     }
 }

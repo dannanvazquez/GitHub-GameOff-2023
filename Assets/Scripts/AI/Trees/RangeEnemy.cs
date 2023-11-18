095e9494d5f0e145418db9bff8ec1f5fce7d3ced
BRS0T <spam.brst@gmail.com>
2023-11-18 12:51:17 +0100

    Toolbar basic UI sfx ; worman missile sfx (WIP)

--- Assets/Scripts/AI/Trees/RangeEnemy.cs
+++ Assets/Scripts/AI/Trees/RangeEnemy.cs
@@ -12,6 +12,25 @@ public class RangeEnemy : EnemyAI {
     [Tooltip("The initial force that the projectile of the basic attack is thrown at.")]
     [SerializeField] private float projectileForce;
 
+    [Header("Sounds")]
+    [SerializeField] private AudioSource AudioSource_missile;
+    [SerializeField] private AudioClip[] missile_charge_sfx;
+    //[SerializeField] private AudioClip[] missile_shoot_sfx;    
+    [SerializeField] private AudioClip[] missile_travel_sfx;    
+
+    // --AUDIO-- // 
+    private void PlayRandomClip(AudioClip[] clips, AudioSource audioSource)
+    {
+        if (clips.Length > 0)
+        {
+            AudioClip clip;
+            clip = clips[UnityEngine.Random.Range(0, clips.Length)];
+            audioSource.clip = clip;
+            audioSource.pitch = UnityEngine.Random.Range(.95f, 1.05f);
+            audioSource.Play();
+        }
+    }
+
     public override void ConstructBehaviorTree() {
         OffCooldownNode shootCooldownNode = new OffCooldownNode(this, basicAttackCooldown);
 
@@ -35,14 +54,18 @@ public class RangeEnemy : EnemyAI {
     public IEnumerator LoadProjectile() {
         yield return new WaitForSeconds(basicAttackCooldown);
         loadedProjectile = Instantiate(projectilePrefab, projectileSpawnTransform);
+        PlayRandomClip(missile_charge_sfx, AudioSource_missile);
     }
 
     public void RangeAttack() {
         loadedProjectile.transform.parent = null;
+        // Get the AudioSource component from the instantiated bread and play the spawnbread_sfx on throw
+        AudioSource missileAudioSource = loadedProjectile.GetComponent<AudioSource>();
         loadedProjectile.transform.rotation = Quaternion.LookRotation((playerTransform.position - loadedProjectile.transform.position).normalized);
         loadedProjectile.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         loadedProjectile.GetComponent<Rigidbody>().AddForce(loadedProjectile.transform.forward * projectileForce, ForceMode.Impulse);
         loadedProjectile = null;
+        PlayRandomClip(missile_travel_sfx, missileAudioSource);
     }
 
     public void SpecialAttack() {
--- Assets/Scripts/ItemButton.cs
+++ Assets/Scripts/ItemButton.cs
@@ -9,8 +9,15 @@ public class ItemButton : MonoBehaviour {
     [SerializeField] private Image slotImage;
     [SerializeField] private TMP_Text ammoText;
 
+    [SerializeField] private AudioSource AudioSource_UI;
+    [SerializeField] private AudioClip clic_ui;
+
     public void ToggleSlotSelection(bool isSelected) {
         slotImage.sprite = isSelected ? selectedSlotSprite : normalSlotSprite;
+        
+        AudioSource_UI.pitch = UnityEngine.Random.Range(.9f, 1.1f);
+        AudioSource_UI.PlayOneShot(clic_ui);
+        
     }
 
     public void SetAmmo(float currentAmmo, float maxAmmo) {

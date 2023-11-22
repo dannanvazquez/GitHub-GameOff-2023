1f724190d20fdd7c530b9c9bb12b27be3831b203
BRS0T <spam.brst@gmail.com>
2023-11-22 21:46:03 +0100

    Loot Manager and Chests

--- Assets/Scripts/Interactable Elements/Chest.cs
+++ Assets/Scripts/Interactable Elements/Chest.cs
@@ -1,54 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-public class Chest : MonoBehaviour
-{
-    [SerializeField] private AudioSource AudioSource_opening;
+    public class Chest : MonoBehaviour
+    {
+        [SerializeField] private AudioSource AudioSource_opening;
         [SerializeField] private AudioSource AudioSource_closing;
-    [Header("Audioclips")] 
-    [Tooltip("opening")]  
-    [SerializeField] private AudioClip[] chest_opening_sfx;
-    [Tooltip("closing")]       
-    [SerializeField] private AudioClip[] chest_closing_sfx;    
+        [Header("Audioclips")] 
+        [Tooltip("opening")]  
+        [SerializeField] private AudioClip[] chest_opening_sfx;
+        [Tooltip("closing")]       
+        [SerializeField] private AudioClip[] chest_closing_sfx;    
 
+        private bool hasOpened = false;
+        private LootManager lootManager;
 
-    private Animator anim;
-    // Start is called before the first frame update
-    void Start()
-    {
-        anim = this.gameObject.GetComponent<Animator>();
-    }
-
-    private void PlayRandomClip(AudioClip[] clips, AudioSource audioSource)
-    {
-            AudioClip clip;
-            clip = clips[UnityEngine.Random.Range(0, clips.Length)];
-            audioSource.clip = clip;
-            audioSource.pitch = UnityEngine.Random.Range(.95f, 1.15f);
-            audioSource.Play();
-            
-        }
-
-
-    // Update is called once per frame
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
+        private Animator anim;
+        // Start is called before the first frame update
+        void Start()
         {
-            anim.SetBool("OpenChest", true);
-            Debug.Log("Test<color=red> chest should open");
-            PlayRandomClip(chest_opening_sfx, AudioSource_opening);
-
+            anim = this.gameObject.GetComponent<Animator>();
+            lootManager = this.gameObject.GetComponent<LootManager>();
         }
-    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
+        private void PlayRandomClip(AudioClip[] clips, AudioSource audioSource)
         {
-            anim.SetBool("OpenChest", false);
-            PlayRandomClip(chest_closing_sfx, AudioSource_closing);
+                AudioClip clip;
+                clip = clips[UnityEngine.Random.Range(0, clips.Length)];
+                audioSource.clip = clip;
+                audioSource.pitch = UnityEngine.Random.Range(.95f, 1.15f);
+                audioSource.Play();
+                
+            }
+
+
+        // Update is called once per frame
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                anim.SetBool("OpenChest", true);
+                Debug.Log("Test<color=red> chest should open");
+                PlayRandomClip(chest_opening_sfx, AudioSource_opening);
+                // Instantiate loot on the first opening
+
+                lootManager.InstantiateRandomLoot(transform.position);
+                
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                anim.SetBool("OpenChest", false);
+                PlayRandomClip(chest_closing_sfx, AudioSource_closing);
+                // Destroy the loot when closing the chest
+
+                lootManager.DestroyLoot();
+            }
         }
     }
-}
---
+++ Assets/Scripts/Interactable Elements/LootManager.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootManager : MonoBehaviour
+{
+    public GameObject firstLootPrefab;
+    public GameObject secondLootPrefab;
+    public GameObject thirdLootPrefab;
+    public GameObject fourthLootPrefab;
+    public GameObject fifthLootPrefab;
+    public GameObject sixthLootPrefab;
+    public GameObject seventhLootPrefab;
+
+    private GameObject currentLoot;
+    private Vector3 originalScale;
+
+    public void InstantiateRandomLoot(Vector3 chestPosition)
+    {
+        // Check if there's an existing loot, and if not, instantiate a new one
+        if (currentLoot == null)
+        {
+            int randomIndex = Random.Range(0, 7); // 0-6: firstLoot, 1-6: secondLoot, 2-6: thirdLoot, 3-6: fourthLoot, 4-6: fifthLoot, 5-6: sixthLoot, 6: seventhLoot
+
+            GameObject lootPrefab = null;
+
+            switch (randomIndex)
+            {
+                case 0:
+                    lootPrefab = firstLootPrefab;
+                    break;
+                case 1:
+                    lootPrefab = secondLootPrefab;
+                    break;
+                case 2:
+                    lootPrefab = thirdLootPrefab;
+                    break;
+                case 3:
+                    lootPrefab = fourthLootPrefab;
+                    break;
+                case 4:
+                    lootPrefab = fifthLootPrefab;
+                    break;
+                case 5:
+                    lootPrefab = sixthLootPrefab;
+                    break;
+                case 6:
+                    lootPrefab = seventhLootPrefab;
+                    break;
+            }
+
+            if (lootPrefab != null)
+            {
+                Vector3 adjustedPosition = chestPosition + new Vector3(0f, 1.5f, -0.28f);
+
+                // Instantiate new loot
+                currentLoot = Instantiate(lootPrefab, adjustedPosition, Quaternion.identity);
+                
+                // Save the original scale
+                originalScale = currentLoot.transform.localScale;
+
+                // Adjust the scale of the instantiated loot
+                StartCoroutine(ScaleOverTime(currentLoot.transform, new Vector3(0f, 0f, 0f), originalScale, 1.0f));
+            }
+        }
+    }
+
+public void DestroyLoot()
+{
+    // Check if there's an existing loot, and if so, destroy it
+    if (currentLoot != null)
+    {
+        StartCoroutine(DestroyLootWithDelay());
+    }
+}
+
+IEnumerator DestroyLootWithDelay()
+{
+    // Add a delay before scaling down and destroying the loot
+    yield return new WaitForSeconds(.5f);
+
+    // Scale down the loot before destroying
+    StartCoroutine(ScaleOverTime(currentLoot.transform, currentLoot.transform.localScale, new Vector3(0f, 0f, 0f), 1.0f));
+    
+    // Add a delay before destroying to allow scaling animation
+    yield return new WaitForSeconds(1.0f);
+
+    // Destroy the loot
+    Destroy(currentLoot);
+
+    // Set currentLoot to null after destruction
+    currentLoot = null;
+}
+    IEnumerator ScaleOverTime(Transform objectToScale, Vector3 fromScale, Vector3 toScale, float duration)
+    {
+        float startTime = Time.time;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            objectToScale.localScale = Vector3.Lerp(fromScale, toScale, elapsedTime / duration);
+            elapsedTime = Time.time - startTime;
+            yield return null;
+        }
+
+        // Ensure the final scale is exactly as intended
+        objectToScale.localScale = toScale;
+    }
+}

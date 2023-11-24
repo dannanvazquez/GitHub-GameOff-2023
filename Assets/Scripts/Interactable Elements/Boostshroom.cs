a03e1e2ed292bd18148a1be6182431f2fcb4b5ac
BRS0T <spam.brst@gmail.com>
2023-11-24 11:55:00 +0100

    Added jumpershroom - still need to ajust gravity ; little chest correction

--- Assets/Scripts/Interactable Elements/Boostshroom.cs
+++ Assets/Scripts/Interactable Elements/Boostshroom.cs
@@ -7,10 +7,14 @@ public class Boostshroom : MonoBehaviour
     [Tooltip("The amount of force to apply to the colliding rigidbody.")]
     public float yForce = 1.5f;
 
+    private Animator animator;
+    private bool hasAnimationPlayed = false;
+    [SerializeField] private AudioSource jumper_audiosource;
+    [SerializeField] private AudioClip jump_mushroom_sfx;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,10 +25,33 @@ public class Boostshroom : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.tag != "Player") return;
-        if (collision.body == null) return;
+        if (!hasAnimationPlayed && collision.transform.CompareTag("Player"))
+        {
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddForce(new Vector3(0, yForce * 1000, 0));
 
-        Rigidbody rb = (Rigidbody)collision.body;
-        rb.AddForce(new Vector3(0, yForce * 1000, 0));
+                // Play the "AnimMushroomActivate" animation
+                if (animator != null)
+                {
+                    animator.SetTrigger("AnimMushroomActivateTrigger");
+                    hasAnimationPlayed = true; // Set the flag to true after playing the animation
+                    jumper_audiosource.pitch=Random.Range(0.95f, 1.05f);
+                    jumper_audiosource.PlayOneShot(jump_mushroom_sfx);
+
+
+                }
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            // Reset the flag when the player exits
+            hasAnimationPlayed = false;
+        }
     }
 }
--- Assets/Scripts/Interactable Elements/LootManager.cs
+++ Assets/Scripts/Interactable Elements/LootManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LootManager : MonoBehaviour
 {
@@ -10,17 +12,65 @@ public class LootManager : MonoBehaviour
     public GameObject fifthLootPrefab;
     public GameObject sixthLootPrefab;
     public GameObject seventhLootPrefab;
-    
+
     private GameObject currentLoot;
     private Vector3 originalScale;
+
+    public TextMeshProUGUI lootNameText; // Reference to the TextMeshProUGUI component
+    public Canvas lootCanvas; // Reference to the Canvas component
+
+    private bool isCanvasVisible = false;
+
+    void Start()
+    {
+        // Ensure the canvas is initially hidden
+        if (lootCanvas != null)
+        {
+            lootCanvas.enabled = false;
+        }
+    }
 
     public void InstantiateRandomLoot(Vector3 chestPosition)
     {
         // Check if there's an existing loot, and if not, instantiate a new one
         if (currentLoot == null)
         {
-            int randomIndex = Random.Range(0, 7);
-            GameObject lootPrefab = GetLootPrefab(randomIndex);
+            int randomIndex = Random.Range(0, 7); // 0-6: firstLoot, 1-6: secondLoot, 2-6: thirdLoot, 3-6: fourthLoot, 4-6: fifthLoot, 5-6: sixthLoot, 6: seventhLoot
+
+            GameObject lootPrefab = null;
+            string lootName = "";
+
+            switch (randomIndex)
+            {
+                case 0:
+                    lootPrefab = firstLootPrefab;
+                    lootName = "First Loot";
+                    break;
+                case 1:
+                    lootPrefab = secondLootPrefab;
+                    lootName = "Second Loot";
+                    break;
+                case 2:
+                    lootPrefab = thirdLootPrefab;
+                    lootName = "Third Loot";
+                    break;
+                case 3:
+                    lootPrefab = fourthLootPrefab;
+                    lootName = "Fourth Loot";
+                    break;
+                case 4:
+                    lootPrefab = fifthLootPrefab;
+                    lootName = "Fifth Loot";
+                    break;
+                case 5:
+                    lootPrefab = sixthLootPrefab;
+                    lootName = "Sixth Loot";
+                    break;
+                case 6:
+                    lootPrefab = seventhLootPrefab;
+                    lootName = "Seventh Loot";
+                    break;
+            }
 
             if (lootPrefab != null)
             {
@@ -29,8 +79,21 @@ public class LootManager : MonoBehaviour
                 // Instantiate new loot
                 currentLoot = Instantiate(lootPrefab, adjustedPosition, Quaternion.identity);
 
+                // Update the TextMeshProUGUI component with the loot name
+                if (lootNameText != null)
+                {
+                    lootNameText.text = lootName;
+                }
+
                 // Save the original scale
                 originalScale = currentLoot.transform.localScale;
+
+                // Show the canvas
+                if (lootCanvas != null)
+                {
+                    lootCanvas.enabled = true;
+                    isCanvasVisible = true;
+                }
 
                 // Adjust the scale of the instantiated loot
                 StartCoroutine(ScaleOverTime(currentLoot.transform, new Vector3(0f, 0f, 0f), originalScale, 1.0f));
@@ -47,6 +110,16 @@ public class LootManager : MonoBehaviour
         }
     }
 
+    public void LootObject()
+    {
+        // Hide the canvas when the player loots the object
+        if (isCanvasVisible && lootCanvas != null)
+        {
+            lootCanvas.enabled = false;
+            isCanvasVisible = false;
+        }
+    }
+
     IEnumerator DestroyLootWithDelay()
     {
         // Add a delay before scaling down and destroying the loot
@@ -55,42 +128,22 @@ public class LootManager : MonoBehaviour
         // Scale down the loot before destroying
         StartCoroutine(ScaleOverTime(currentLoot.transform, currentLoot.transform.localScale, new Vector3(0f, 0f, 0f), 1.0f));
 
-
-
         // Add a delay before destroying to allow scaling animation
         yield return new WaitForSeconds(1.0f);
+
+        // Hide the canvas
+        if (lootCanvas != null)
+        {
+            lootCanvas.enabled = false;
+            isCanvasVisible = false;
+        }
 
         // Destroy the loot
         Destroy(currentLoot);
 
-        // Set currentLoot and currentLootParticles to null after destruction
+        // Set currentLoot to null after destruction
         currentLoot = null;
-
     }
-
-    private GameObject GetLootPrefab(int index)
-    {
-        switch (index)
-        {
-            case 0:
-                return firstLootPrefab;
-            case 1:
-                return secondLootPrefab;
-            case 2:
-                return thirdLootPrefab;
-            case 3:
-                return fourthLootPrefab;
-            case 4:
-                return fifthLootPrefab;
-            case 5:
-                return sixthLootPrefab;
-            case 6:
-                return seventhLootPrefab;
-            default:
-                return null;
-        }
-    }
-
 
     IEnumerator ScaleOverTime(Transform objectToScale, Vector3 fromScale, Vector3 toScale, float duration)
     {

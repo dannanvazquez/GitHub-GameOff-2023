4ba3f0969a85c97eaeb5e02daf05a9496328b74a
BRS0T <spam.brst@gmail.com>
2023-11-30 14:18:23 +0100

    Chests polish

---
+++ Assets/Scripts/Interactable Elements/ChestUpgrades.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestUpgrades : MonoBehaviour
+{
+    [SerializeField] private AudioSource AudioSource_opening;
+    [SerializeField] private AudioSource AudioSource_closing;
+    [Header("Audioclips")]
+    [Tooltip("opening")]
+    [SerializeField] private AudioClip[] chest_opening_sfx;
+    [Tooltip("closing")]
+    [SerializeField] private AudioClip[] chest_closing_sfx;
+
+    private bool hasOpened = false;
+    private LootManagerUpgrades lootManager;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+        lootManager = this.gameObject.GetComponent<LootManagerUpgrades>();
+    }
+
+    private void PlayRandomClip(AudioClip[] clips, AudioSource audioSource)
+    {
+        AudioClip clip;
+        clip = clips[UnityEngine.Random.Range(0, clips.Length)];
+        audioSource.clip = clip;
+        audioSource.pitch = UnityEngine.Random.Range(.95f, 1.15f);
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !hasOpened)
+        {
+            anim.SetBool("OpenChest", true);
+            Debug.Log("Test<color=red> chest should open");
+            PlayRandomClip(chest_opening_sfx, AudioSource_opening);
+            // Instantiate loot on the first opening
+            lootManager.InstantiateRandomLoot(transform.position);
+
+            // Set hasOpened to true to indicate that the chest has been opened
+            hasOpened = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //anim.SetBool("OpenChest", false);
+            //PlayRandomClip(chest_closing_sfx, AudioSource_closing);
+            // Destroy the loot when closing the chest
+
+            //lootManager.DestroyLoot();
+
+        }
+    }
+}
---
+++ Assets/Scripts/Interactable Elements/LootManagerUpgrades.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LootManagerUpgrades : MonoBehaviour
+{
+    public GameObject Speed;
+    public GameObject DamageBooster;
+
+    private GameObject currentLoot;
+    private Vector3 originalScale;
+
+    public TextMeshProUGUI lootNameText; // Reference to the TextMeshProUGUI component
+    public Canvas lootCanvas; // Reference to the Canvas component
+
+    private bool isCanvasVisible = false;
+
+    void Start()
+    {
+        // Ensure the canvas is initially hidden
+        if (lootCanvas != null)
+        {
+            lootCanvas.enabled = false;
+        }
+    }
+
+    public void InstantiateRandomLoot(Vector3 chestPosition)
+    {
+        // Check if there's an existing loot, and if not, instantiate a new one
+        if (currentLoot == null)
+        {
+            int randomIndex = Random.Range(0, 1); 
+
+            GameObject lootPrefab = null;
+            string lootName = "";
+
+            switch (randomIndex)
+            {
+                case 0:
+                    lootPrefab = Speed;
+                    lootName = "Speed";
+                    break;
+                case 1:
+                    lootPrefab = DamageBooster;
+                    lootName = "Damage Booster";
+                    break;
+            }
+
+            if (lootPrefab != null)
+            {
+                Vector3 adjustedPosition = chestPosition + new Vector3(0f, 2.5f, -0.28f);
+
+                // Instantiate new loot
+                currentLoot = Instantiate(lootPrefab, adjustedPosition, Quaternion.identity);
+
+                // Update the TextMeshProUGUI component with the loot name
+                if (lootNameText != null)
+                {
+                    lootNameText.text = lootName;
+                }
+
+                // Save the original scale
+                originalScale = currentLoot.transform.localScale;
+
+                // Show the canvas
+                if (lootCanvas != null)
+                {
+                    lootCanvas.enabled = true;
+                    isCanvasVisible = true;
+                }
+
+                // Adjust the scale of the instantiated loot
+                StartCoroutine(ScaleOverTime(currentLoot.transform, new Vector3(0f, 0f, 0f), originalScale, 1.0f));
+            }
+        }
+    }
+
+    public void DestroyLoot()
+    {
+        // Check if there's an existing loot, and if so, destroy it
+        if (currentLoot != null)
+        {
+            StartCoroutine(DestroyLootWithDelay());
+        }
+    }
+
+    public void LootObject()
+    {
+        // Hide the canvas when the player loots the object
+        if (isCanvasVisible && lootCanvas != null)
+        {
+            lootCanvas.enabled = false;
+            isCanvasVisible = false;
+        }
+    }
+
+    IEnumerator DestroyLootWithDelay()
+    {
+        // Add a delay before scaling down and destroying the loot
+        yield return new WaitForSeconds(.5f);
+
+        // Scale down the loot before destroying
+        StartCoroutine(ScaleOverTime(currentLoot.transform, currentLoot.transform.localScale, new Vector3(0f, 0f, 0f), 1.0f));
+
+        // Add a delay before destroying to allow scaling animation
+        yield return new WaitForSeconds(1.0f);
+
+        // Hide the canvas
+        if (lootCanvas != null)
+        {
+            lootCanvas.enabled = false;
+            isCanvasVisible = false;
+        }
+
+        // Destroy the loot
+        Destroy(currentLoot);
+
+        // Set currentLoot to null after destruction
+        currentLoot = null;
+    }
+
+    IEnumerator ScaleOverTime(Transform objectToScale, Vector3 fromScale, Vector3 toScale, float duration)
+    {
+        float startTime = Time.time;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            objectToScale.localScale = Vector3.Lerp(fromScale, toScale, elapsedTime / duration);
+            elapsedTime = Time.time - startTime;
+            yield return null;
+        }
+
+        // Ensure the final scale is exactly as intended
+        objectToScale.localScale = toScale;
+    }
+}

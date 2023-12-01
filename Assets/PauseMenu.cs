688c3f47fc37d5a7d5680707c5abf913169c3a15
BRS0T <spam.brst@gmail.com>
2023-12-01 02:46:42 +0100

    Muffled sound when pause menu is active

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
-
+    [Header("Mixer")]
+    [SerializeField] private AudioMixer myAudioMixer;
+    [Range(200,8000)][SerializeField] private float muffled=3000;
+    [Range(17000,22000)][SerializeField] private float not_muffled=22000;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
@@ -17,9 +20,14 @@ public class PauseMenu : MonoBehaviour
     void TogglePauseMenu()
     {
         // Toggle the visibility of the pause menu
-        pauseMenu.SetActive(!pauseMenu.activeSelf);
+        bool isMenuActive = !pauseMenu.activeSelf;
+        pauseMenu.SetActive(isMenuActive);
+
+        // Pause or resume the game time based on the menu's visibility
+        Time.timeScale = isMenuActive ? 0 : 1;
 
         // Pause or resume the game time based on the menu's visibility
         Time.timeScale = (pauseMenu.activeSelf) ? 0 : 1;
+        myAudioMixer.SetFloat("MUFFLED_SOUND", isMenuActive ? muffled : not_muffled);
     }
 }
